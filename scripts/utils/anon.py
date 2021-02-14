a95434b180a0249f8fba411920330cc12ecd173d
Tavernini Gabriel <gabriel.tavernini@buonarroti.tn.it>
2021-02-14 15:32:18 +0100

    Added anonymized data

--- scripts/utils/anon.py
+++ scripts/utils/anon.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import sys
+
+df = pd.read_csv(sys.argv[1])
 
 #keep first three letters
-df = pd.read_csv(sys.argv[1])
-df['user'] = df['user'].apply(lambda x: x if x == 'Point' else x[1:4] if x[0] == '@' else x[:min(3, x.rfind('@'))] if '@' in x else x[:3])
+df['user'] = df['user'].apply(lambda x: x if x == 'Point' else x[0:4] if x[0] == '@' else x[:min(3, x.rfind('@'))] if '@' in x else x[:3])
 
 df.to_csv("locationsAnon.csv", index=False)
